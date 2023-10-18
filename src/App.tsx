11393a2243803bfb7c5ca76af1169186eaf1b3bb
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-17 22:25:22 -0500

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import Editor from "@monaco-editor/react";
 import { setupTypeAcquisition } from '@typescript/ata';
 import ts from 'typescript';
+import * as monacoType from 'monaco-editor';
 
+type CompilerOptions = import("monaco-editor").languages.typescript.CompilerOptions
 export const USER_CODE_PATH = 'file:///user.ts';
+
+// https://github.com/microsoft/TypeScript-Website/blob/652934679e6d0a46f75bb33677bb81f9eee17ed0/packages/sandbox/src/compilerOptions.ts#L10
+const settings: CompilerOptions = {
+  strict: true,
+  noUncheckedIndexedAccess: true,
+  target: monacoType.languages.typescript.ScriptTarget.ES2017,
+  lib: ['es2015', 'dom', 'dom.iterable', 'esnext'],
+  declaration: true,
+  experimentalDecorators: true,
+  emitDecoratorMetadata: true,
+  moduleResolution:
+    monacoType.languages.typescript.ModuleResolutionKind.NodeJs,
+  module: monacoType.languages.typescript.ModuleKind.ESNext,
+  jsx: monacoType.languages.typescript.JsxEmit.React,
+  esModuleInterop: true,
+  allowNonTsExtensions: true,
+}
 
 function App() {
   return (
@@ -11,16 +30,7 @@ function App() {
       height="80vh"
       theme="vs-dark"
       onMount={(editor, monaco) => {
-        monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
-          strict: true,
-          noUncheckedIndexedAccess: true,
-          target: monaco.languages.typescript.ScriptTarget.ESNext,
-          lib: ['es2015', 'dom', 'dom.iterable', 'esnext'],
-          moduleResolution:
-            monaco.languages.typescript.ModuleResolutionKind.NodeJs,
-          module: monaco.languages.typescript.ModuleKind.ESNext,
-          allowNonTsExtensions: true,
-        });
+        monaco.languages.typescript.typescriptDefaults.setCompilerOptions(settings);
 
         // this works
         monaco.languages.typescript.typescriptDefaults.addExtraLib(
@@ -32,12 +42,15 @@ function App() {
           typescript: ts,
           delegate: {
             receivedFile: (code: string, path: string) => {
-              const fileUrl = `file://${path}`;
 
-              console.log("ATA received file", fileUrl );
-              monaco.languages.typescript.typescriptDefaults.addExtraLib(
-                code,
-              );
+              if (path.endsWith(".d.ts")) {
+
+                const fileUrl = `file://${path}`;
+                console.log("ATA received file", fileUrl);
+                monaco.languages.typescript.typescriptDefaults.addExtraLib(
+                  code,
+                );
+              }
 
               // monaco.editor.createModel("", "typescript");
             },
@@ -48,7 +61,7 @@ function App() {
           ata(editor.getValue());
         });
       }}
-      defaultValue={`import React from "react"`}
+      defaultValue={`import { isEven } from "is-even"`}
     />
   )
 }
