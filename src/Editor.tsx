c8eb1d1b37f04b3a45b9abebf0fee7d32fbd1d45
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-10-18 16:58:55 -0500

    checkpoint

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -25,7 +25,7 @@ const settings: CompilerOptions = {
   allowNonTsExtensions: true,
 };
 
-export const CodeEditor = ({ namespace, code }) => {
+export const CodeEditor = ({ namespace, code }: { namespace: string, code: string }) => {
   return (
     <Editor
       defaultPath={namespace}
@@ -37,7 +37,7 @@ export const CodeEditor = ({ namespace, code }) => {
           settings,
         );
 
-        if (monaco.editor.getModel(namespace) === null) {
+        if (monaco.editor.getModel(monaco.Uri.parse(namespace)) === null) {
           const model = monaco.editor.createModel(
             code,
             "typescript",
